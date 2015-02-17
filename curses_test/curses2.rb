177df90e3e4e187c59f3d5cf5ce6077aaceabd28
Toshiyuki Masui <masui@pitecan.com>
2015-02-17 19:08:23 +0900

    part2

---
+++ curses_test/curses2.rb
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+require "curses"
+include Curses
+
+init_screen
+#start_color #!!!!
+
+#init_pair 1, COLOR_YELLOW, COLOR_BLUE
+#init_pair 2, COLOR_BLACK, COLOR_WHITE
+
+stdscr.bkgd color_pair(1)
+stdscr.attrset color_pair(1)
+refresh
+clear
+
+getch
+exit
+
+begin
+  #win = stdscr.subwin(5,30,2,2)
+  # win.box(?|,?-,?*)
+  # bkgd color_pair(1)
+
+  stdscr.setpos 10, 10
+  attrset(color_pair(1)) # Curses.attrset メソッドに Curses.color_pair(id) を渡す
+  stdscr.addstr "サブウィンドウです!!!"
+
+  attrset(color_pair(2)) # Curses.attrset メソッドに Curses.color_pair(id) を渡す
+  stdscr.setpos 12, 10
+  stdscr.addstr "abcdefg"
+
+  # stdscr.refresh
+
+  getch
+ensure
+  close_screen
+end
