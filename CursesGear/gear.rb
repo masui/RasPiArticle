a19b5c84d30f0b808559c3bc5b2346d274c39516
Toshiyuki Masui <masui@pitecan.com>
2015-02-19 07:40:44 +0900

    ...

--- CursesGear/gear.rb
+++ CursesGear/gear.rb
@@ -8,7 +8,7 @@ require 'json'
 require "curses"
 include Curses
 
-init_screen
+#init_screen
 #start_color # これがないと色が出ないorz ... アホな仕様か
 #init_pair 1, COLOR_YELLOW, COLOR_BLUE
 #init_pair 2, COLOR_BLACK, COLOR_WHITE
@@ -16,7 +16,11 @@ init_screen
 #stdscr.attrset color_pair(1)
 #refresh
 #clear
+stdscr.keypad true    # up/downキーの扱い http://rb.blog.pasberth.com/post/27046375001/ruby-curses
+#puts Key::UP
 noecho
+
+@nodelist = []
 
 def readltsv(file)
   root = { 'title' => '全コンテンツ' }
@@ -70,39 +74,70 @@ def prevNode(node)
   prevnode
 end
 
-def calc(centerNode) # centerNodeを中心にnodeListを計算
-  nodelist = {}
-  nodelist[0] = centerNode
-  node = centerNode
+def calc # @centerNodeを中心にnodelistを計算
+  @nodelist = {}
+  @nodelist[0] = @centerNode
+  node = @centerNode
   i = 0
-  nodelist[i+=1] = node while node = nextNode(node)
-  node = centerNode
+  @nodelist[i+=1] = node while node = nextNode(node)
+  node = @centerNode
   i = 0
-  nodelist[i-=1] = node while node = prevNode(node)
-  nodelist
+  @nodelist[i-=1] = node while node = prevNode(node)
+
+  display
 end
 
+#def expand
+#  if @nodelist[0]['children']
+#    calc @nodelist[0].children[0]
+#    expandTimeout = setTimeout expand, StepTime
+
+def cls
+  (0...30).each { |y|
+    stdscr.setpos y, 0
+    stdscr.addstr "                                                     "
+  }
+end
+
+def display
+  cls
+  @nodelist.each { |key,val|
+    stdscr.setpos 12+key, 3+val['level'].to_i
+    s = val['title']
+    s += "-----" if key == '0'
+    stdscr.addstr val['title'] + key.to_s
+  }
+end
+
+def move(delta)
+  if @nodelist[delta]
+    @centerNode = @nodelist[delta]
+    calc
+  end
+end
+
+#
+# 初期化
+#
 root = readltsv 'contents.ltsv'
 initlinks root, 0
+
+@centerNode = root['children'][0]
+
+calc
+
+#nodelist = calc(centerNode)
 
 #
 # メインループ
 #
 
-centernode = root['children'][0]
-
 while true do
-  calc(centernode).each { |key,val|
-    stdscr.setpos 12+key, 3+val['level'].to_i
-    stdscr.addstr val['title']
-  }
   c = getch
-  puts c
+  if c == Key::UP
+    move 1
+  elsif c == Key::DOWN
+    move -1
+  end
 end
 
-
-calc(root['children'][0]).each { |key,val|
-  puts key
-  puts val['title']
-}
-# puts root['children'][0]['children'][0]['children'][0]['title']
